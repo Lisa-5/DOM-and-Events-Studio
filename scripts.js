2ad2e72a7e9249e4877ea77c8279055d3df7dbd4
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-01-31 21:08:38 -0600

    worked on studio in cclass got to 2b

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+// window.addEventListener("load", function() {
+//      // put DOM code here to ensure elements have been loaded
+//     console.log('window loaded');
+//     const takeOff = document.getElementById ("takeoff");
+//     let flightStatus = document.querySelector("#flightStatus");
+//     console.log(takeOff);
+//     takeOff.addEventListener('click', event => {window.confirm("Confirm that the shuttle is ready for takeoff.");
+//     flightStatus.innerHTML = "<p>Shuttle in flight.</p>";
+// })
+// })
+window.addEventListener("load", function() {
+console.log('window loaded');
+    const takeOff = document.getElementById ("takeoff");
+    let flightStatus = document.querySelector("#flightStatus");
+    console.log(takeOff);
+    takeOff.addEventListener('click', event => {window.confirm("Confirm that the shuttle is ready for takeoff.");
+    flightStatus.innerHTML = "<p>Shuttle in flight.</p>";
+})
+})
